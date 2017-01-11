57f5e9fbd281a6323644bcb99594bd473ef43aac
Cory Forward <cory@linux.com>
2017-01-10 21:37:55 -0700

    Implemented utility function to easily create reducers

--- src/renderer/redux/modules/TodoStore.js
+++ src/renderer/redux/modules/TodoStore.js
@@ -1,5 +1,7 @@
-import * as _ from "lodash"
-import Immutable from "immutable"
+import * as _ from "lodash";
+import Immutable from "immutable";
+
+import { createReducer } from "../../util";
 
 const TODO_ADD = "TODO_ADD";
 const TODO_REMOVE = "TODO_REMOVE";
@@ -28,62 +30,51 @@ function getInitialState() {
     }
 }
 
-export default function reduce(state=getInitialState(), action) {
-
-    const type = action.type;
-
-    switch ( type ) {
-        case TODO_ADD: {
-            /**
-             * Adds a new todo to the state,
-             * with the text specified by the payload
-             */
-            let todo = new Todo(action.text);
-            return {
-                ...state,
-                todos: state.todos.push(todo)
-            }
+export default createReducer(getInitialState(), {
+    [TODO_ADD](state, { text }) {
+        /**
+         * Adds a new todo to the state,
+         * with the text specified by the payload
+         */
+        let todo = new Todo(text);
+        return {
+            ...state,
+            todos: state.todos.push(todo)
         }
-        case TODO_TOGGLE_COMPLETE: {
-            /**
-             * Toggles the complete status of the todo with the 
-             * id specified by the action
-             */
-            return {
-                ...state,
-                todos: state.todos.map((todo) => {
-                    var complete = 
-                        (todo.id == action.id) ? !(todo.complete) : todo.complete;
-                    return {
-                        ...todo,
-                        complete
-                    }
-                })
-            }
+    },
+    [TODO_TOGGLE_COMPLETE](state, { id }) {
+        return {
+            ...state,
+            todos: state.todos.map((todo) => {
+                var complete = 
+                    (todo.id == id) ? !(todo.complete) : todo.complete;
+                return {
+                    ...todo,
+                    complete
+                }
+            })
         }
-        case TODO_REMOVE: {
-            /**
-             * Removes the todo with the specified id from
-             *  the current state
-             */
-            return {
-                ...state,
-                todos: state.todos.filterNot((todo) => todo.id == action.id)
-            }
+    },
+    [TODO_REMOVE](state, { id }) {
+        /**
+         * Removes the todo with the specified id from
+         *  the current state
+         */
+        return {
+            ...state,
+            todos: state.todos.filterNot((todo) => todo.id == id)
         }
-        case TODO_REMOVE_COMPLETE: {
-            /**
-             * Filters out todos that are not complete yet
-             */
-            return {
-                ...state,
-                todos: state.todos.filterNot(todo => todo.complete)
-            }
+    },
+    [TODO_REMOVE_COMPLETE](state) {
+        /**
+         * Filters out todos that are not complete yet
+        */
+        return {
+            ...state,
+            todos: state.todos.filterNot(todo => todo.complete)
         }
     }
-
-    return state;
-}
+});
 
 export function addTodo(text) {
     return {
